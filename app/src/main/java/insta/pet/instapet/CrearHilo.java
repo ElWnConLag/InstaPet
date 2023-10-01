ae0992caa883d0ed92743b70bee0887be9d03747
ElWnConLag <uribematias.2002@gmail.com>
2023-10-01 20:48:56 -0300

    coso3

---
+++ app/src/main/java/insta/pet/instapet/CrearHilo.java
@@ -0,0 +1,14 @@
+package insta.pet.instapet;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CrearHilo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_hilo);
+    }
+}
