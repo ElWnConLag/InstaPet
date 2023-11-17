0c9a061f35888c73848c611fb897fd627e3b575a
JasonLeppin <Jasonmaquehue15@outlook.es>
2023-11-17 03:45:56 -0300

    Implementacion de seguir y dejar de seguir
    implementacion de base de datos al seguido y dejar de seguir

--- app/src/main/java/insta/pet/instapet/L_PerfilDueno.java
+++ app/src/main/java/insta/pet/instapet/L_PerfilDueno.java
@@ -1,13 +1,17 @@
 package insta.pet.instapet;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -15,12 +19,47 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class L_PerfilDueno extends AppCompatActivity {
 
     private TextView nombreperfil1;
     private ImageView imagenperfil1;
     private Button botonSeguirPerfil;
-    private TextView buttonseguidos;
+    private DatabaseReference followReference;
+    private DatabaseReference userReference;
+    private FirebaseUser currentUser;
+
+    public class User {
+        private int followers;
+        private int following;
+
+
+        public User(int followers, int following) {
+            this.followers = followers;
+            this.following = following;
+        }
+
+        public int getFollowers() {
+            return followers;
+        }
+
+        public void setFollowers(int followers) {
+            this.followers = followers;
+            setFollowers(10);
+        }
+
+        public int getFollowing() {
+            return following;
+        }
+
+        public void setFollowing(int following) {
+            this.following = following;
+            setFollowing(5);
+        }
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,12 +68,110 @@ public class L_PerfilDueno extends AppCompatActivity {
 
         nombreperfil1 = findViewById(R.id.textViewUsername);
         imagenperfil1 = findViewById(R.id.imageView3);
+        botonSeguirPerfil = findViewById(R.id.botonSeguirPerfil);
 
-        // Inicializa la referencia a Firebase Realtime Database
+        followReference = FirebaseDatabase.getInstance().getReference().child("follows");
+        userReference = FirebaseDatabase.getInstance().getReference().child("users");
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        currentUser = auth.getCurrentUser();
+
+        botonSeguirPerfil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isFollowing = botonSeguirPerfil.getText().toString().equals("Seguir");
+
+                if (isFollowing) {
+                    followUser();
+                } else {
+                    unfollowUser();
+                }
+            }
+        });
+
+        checkFollowingStatus();
+        displayFollowCounts();
+        loadUserData();
+    }
+
+    private void followUser() {
+        followReference.child(currentUser.getUid()).child("following").setValue(true);
+        updateFollowCounts(1);
+        Toast.makeText(L_PerfilDueno.this, "Dejando de seguir al usuario", Toast.LENGTH_SHORT).show();
+    }
+
+    private void unfollowUser() {
+        followReference.child(currentUser.getUid()).child("following").setValue(false);
+        updateFollowCounts(-1);
+        Toast.makeText(L_PerfilDueno.this, "Siguiendo al usuario", Toast.LENGTH_SHORT).show();
+    }
+
+    private void updateFollowCounts(final int increment) {
+        userReference.child(currentUser.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                if (user != null) {
+                    user.setFollowers(user.getFollowers() + increment);
+                    user.setFollowing(user.getFollowing() + increment);
+
+                    userReference.child(currentUser.getUid()).setValue(user);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Manejar errores si es necesario
+            }
+        });
+    }
+
+    private void checkFollowingStatus() {
+        followReference.child(currentUser.getUid()).child("following").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                Boolean following = dataSnapshot.getValue(Boolean.class);
+
+                if (following != null) {
+                    botonSeguirPerfil.setText(following ? "Dejar de seguir" : "Seguir");
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Manejar errores si es necesario
+            }
+        });
+    }
+
+    private void displayFollowCounts() {
+        userReference.child(currentUser.getUid()).addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                User user = dataSnapshot.getValue(User.class);
+
+                if (user != null) {
+                    int followersCount = user.getFollowers();
+                    int followingCount = user.getFollowing();
+
+                    // Ejemplo: Actualizar TextViews
+                    // followersTextView.setText("Seguidores: " + followersCount);
+                    // followingTextView.setText("Siguiendo: " + followingCount);
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Manejar errores si es necesario
+            }
+        });
+    }
+
+    private void loadUserData() {
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("DatosUsuario");
 
-        // Lee los datos del nombre de usuario y la URL de la imagen desde la base de datos
         myRef.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
@@ -42,9 +179,12 @@ public class L_PerfilDueno extends AppCompatActivity {
                     String nombreUsuario = dataSnapshot.child("nombre").getValue(String.class);
                     String imageUrl = dataSnapshot.child("imagenPerfil").getValue(String.class);
 
-                    // Configura la imagen y el nombre de usuario en los elementos ImageView y TextView
                     Picasso.get().load(imageUrl).into(imagenperfil1);
                     nombreperfil1.setText(nombreUsuario);
+
+                    List<String> usuarios = new ArrayList<>();
+
+                    // Resto del código...
                 }
             }
 
@@ -52,15 +192,6 @@ public class L_PerfilDueno extends AppCompatActivity {
             public void onCancelled(@NonNull DatabaseError databaseError) {
                 // Manejar errores de lectura de la base de datos si es necesario
             }
-
-            class secondActivity extends AppCompatActivity {
-                @Override
-                protected void onCreate(Bundle savedInstanceState) {
-                    super.onCreate(savedInstanceState);
-                    setContentView(R.layout.l_perfildueno_act);
-                }
-            }
         });
-
     }
 }
